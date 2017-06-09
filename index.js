6e9a56c5782d78f452f3ae1a3d1c98cbf490c90a
vorg <marcin.ignac@gmail.com>
2017-06-09 16:55:45 +0100

    Implement pinch to zoom

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function createSpace (w, h) {
     clickTime: 0,
     prevOffset: [0, 0], // offset before at mouse down moment
     changed: new Signal(),
-    dragging: false
+    panning: false,
+    oneFingerZooming: false
   }
 
   // On mouse down the following scenarios can occur
@@ -26,28 +27,44 @@ function createSpace (w, h) {
   // into consideration
   // D: if tap occured quicly after the previous one we might enter
   // one finger zoom mode TODO: implement tap scale
-  space.down = (x, y) => {
+  space.down = (x, y, shift, touches) => {
     console.log('down', x, y)
     const pos = [x, y]
-
     // we will enable one finger zoom on fast double click and drag
     const timeSinceLastClick = Date.now() - space.clickTime
     const distanceFromLastClick = Vec2.distance(pos, space.clickPos)
     console.log('down since', timeSinceLastClick, distanceFromLastClick)
-    if (timeSinceLastClick < 500 && distanceFromLastClick < space.size[0] / 10) {
-      space.zooming = true
+    if (touches && (touches.length === 2)) {
+      console.log('pinch!')
+      // Convert touches positions to space coords
+      // On the next move convert finger positions to space coords
+      // and calculate how much the scale should change to move
+      // old position to the new one
+      // TODO: is the finger order always the
+      // sort just in case, so the order is consistient
+      const touchesInSpace = touches.sort((a, b) => a.id - b.id).map((touch) => {
+        return { id: touch.id, screenPosition: [touch.x, touch.y], spacePosition: space.fromScreenCoords([touch.x, touch.y]) }
+      })
+      space.pinchZooming = true
+      space.oneFingerZooming = false
+      space.panning = false
+      space.oldScale = space.scale
+      space.touches = touchesInSpace
+    } else if (timeSinceLastClick < 500 && distanceFromLastClick < space.size[0] / 10) {
+      space.oneFingerZooming = true
     } else {
-      space.dragging = true
+      space.panning = true
     }
     Vec2.set(space.prevOffset, space.offset)
     Vec2.set(space.clickPos, pos)
     Vec2.set(space.prevPos, pos)
     space.clickTime = Date.now()
+    space.changed.dispatch(space)
   }
 
-  space.move = (x, y) => {
+  space.move = (x, y, shift, touches) => {
     const pos = [x, y]
-    if (space.dragging) {
+    if (space.panning) {
       const delta = Vec2.sub(Vec2.copy(pos), space.clickPos)
       Vec2.scale(delta, 1 / space.scale)
       Vec2.set(space.offset, space.prevOffset)
@@ -55,24 +72,53 @@ function createSpace (w, h) {
 
       space.changed.dispatch(space)
     }
-    if (space.zooming) {
+    if (space.oneFingerZooming) {
       const delta = Vec2.sub(Vec2.copy(pos), space.prevPos)
-      space.scroll(space.clickPos[0], space.clickPos[1], -delta[1])
+      space.scroll(space.clickPos[0], space.clickPos[1], delta[1])
       space.changed.dispatch(space)
     }
+    if (space.pinchZooming) {
+      const touchesInSpace = touches.sort((a, b) => a.id - b.id).map((touch) => {
+        return { id: touch.id, screenPosition: [touch.x, touch.y], spacePosition: space.fromScreenCoords([touch.x, touch.y]) }
+      })
+      const prevPosA = space.touches[0].screenPosition
+      const prevPosB = space.touches[1].screenPosition
+      const posA = touchesInSpace[0].screenPosition
+      const posB = touchesInSpace[1].screenPosition
+      const prevCenter = Vec2.scale(Vec2.add(Vec2.copy(prevPosA), prevPosB), 0.5)
+      const center = Vec2.scale(Vec2.add(Vec2.copy(posA), posB), 0.5)
+      const oldDist = Vec2.distance(prevPosA, prevCenter)
+      const dist = Vec2.distance(posA, center)
+      const zoom = dist / oldDist
+
+      const zoomPoint = space.fromScreenCoords(prevCenter)
+      space.scale = space.oldScale * zoom
+      const driftedMousePos = space.toScreenCoords(zoomPoint)
+
+      const delta = Vec2.sub(Vec2.copy(driftedMousePos), prevCenter)
+      Vec2.scale(delta, 1 / space.scale)
+      Vec2.sub(space.offset, delta)
+
+      space.changed.dispatch(space)
+    }
+    // prevent one finger zoom to kick in when panning
+    space.clickTime = 0
+
     Vec2.set(space.prevPos, pos)
   }
 
   space.up = () => {
     console.log('up')
-    space.dragging = false
-    space.zooming = false
+    space.panning = false
+    space.oneFingerZooming = false
+    space.pinchZooming = false
+    space.touches = null
 
     space.changed.dispatch(space)
   }
 
   // We want scroll to zoom toward the point under the mouse
-  space.scroll = (x, y, dy) => {
+  space.scroll = (x, y, dy, zoom) => {
     console.log('scroll dy', dy)
     const mousePos = [x, y]
     console.log('mousePos', mousePos)
@@ -86,7 +132,7 @@ function createSpace (w, h) {
     // and multiply zoom by around 0.99 .. 1.01 so it works
     // independently of zoom level
     const zoomScale = Math.min(space.size[0], space.size[1]) / 2
-    if (dy > 0) space.scale *= 1.0 + dy / zoomScale
+    if (dy >= 0) space.scale *= 1.0 + dy / zoomScale
     else space.scale *= 1.0 + dy / zoomScale
 
     // Calculate new screen space position of the old mouse pos
@@ -108,14 +154,18 @@ function createSpace (w, h) {
 
   space.fromScreenCoords = (pos) => {
     const spacePos = Vec2.copy(pos)
+    // move mouse pos to the space coords
     Vec2.scale(spacePos, 1 / space.scale)
+    // cancel out the offset
     Vec2.sub(spacePos, space.offset)
     return spacePos
   }
 
   space.toScreenCoords = (pos) => {
     const screenPos = Vec2.copy(pos)
+    // add the offset
     Vec2.add(screenPos, space.offset)
+    // move from space coords to the screen coords
     Vec2.scale(screenPos, space.scale)
     return screenPos
   }
@@ -165,6 +215,32 @@ space.items.push({
   border: '#00FFFF'
 })
 
+space.items.push({
+  position: [-2000, -2000],
+  scale: 1.1,
+  image: 'assets/cover.jpg'
+})
+
+space.items.push({
+  position: [500, -3000],
+  scale: 1.15,
+  image: 'assets/example-draw.jpg'
+})
+
+space.items.push({
+  position: [3500, -1500],
+  scale: 1.12,
+  image: 'assets/reaction-diffusion.jpg'
+})
+
+space.items.forEach((item) => {
+  if (item.image) {
+    const img = new window.Image()
+    img.src = item.image
+    item.image = img
+  }
+})
+
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
 canvas.width = W
@@ -172,10 +248,11 @@ canvas.height = H
 document.body.appendChild(canvas)
 document.body.style.padding = 0
 document.body.style.margin = 0
+let touches = []
 
 // rendering ------------------------------------------------------------------
 
-function redraw() {
+function redraw () {
   // Space is a void with dark background
   ctx.fillStyle = '#333'
   ctx.fillRect(0, 0, W, H)
@@ -192,21 +269,37 @@ function redraw() {
   ctx.lineTo(dx, H)
   ctx.stroke()
   ctx.fillStyle = '#EEE'
-  ctx.fillRect(dx - 5 * scale, dy - 5 * scale, 10 * scale, 10 * scale )
+  ctx.fillRect(dx - 5 * scale, dy - 5 * scale, 10 * scale, 10 * scale)
 
   space.items.forEach((item) => {
     const x = dx + item.position[0] * scale
     const y = dy + item.position[1] * scale
-    const w = item.size[0] * scale
-    const h = item.size[1] * scale
     if (item.background) {
+      const w = item.size[0] * scale
+      const h = item.size[1] * scale
       ctx.fillStyle = item.background
       ctx.fillRect(x, y, w, h)
     } else if (item.border) {
+      const w = item.size[0] * scale
+      const h = item.size[1] * scale
       ctx.strokeStyle = item.border
       ctx.strokeRect(x, y, w, h)
+    } else if (item.image && item.image.width) {
+      const w = item.image.width * item.scale * scale
+      const h = item.image.height * item.scale * scale
+      ctx.drawImage(item.image, x, y, w, h)
     }
   })
+
+  if (touches) {
+    ctx.fillStyle = '#FFF'
+    touches.forEach((touch, i) => {
+      ctx.fillText(`${i} : ${touch.x} ${touch.y}`, 20, 20 + 10 * i)
+    })
+    ctx.fillText(`${space.panning ? 'panning' : ''}`, 20, 20 + 10 * (touches.length + 1))
+    ctx.fillText(`${space.oneFingerZooming ? 'oneFingerZooming' : ''}`, 20, 20 + 10 * (touches.length + 2))
+    ctx.fillText(`${space.pinchZooming ? 'pinchZooming' : ''}`, 20, 20 + 10 * (touches.length + 3))
+  }
 }
 
 // input events ---------------------------------------------------------------
@@ -214,22 +307,39 @@ function redraw() {
 // TODO: add window resize handler e.g. for mobile rotation
 
 function onMouseDown (e) {
+  touches = !e.touches ? null : Array.prototype.slice.call(e.touches).map((touch) => {
+    return {
+      x: touch.clientX,
+      y: touch.clientY,
+      id: touch.identifier
+    }
+  })
   space.down(
     e.offsetX || e.clientX || (e.touches ? e.touches[0].clientX : 0),
     e.offsetY || e.clientY || (e.touches ? e.touches[0].clientY : 0),
-    e.shiftKey || (e.touches && e.touches.length === 2)
+    e.shiftKey || (e.touches && e.touches.length === 2),
+    touches
   )
 }
 
 function onMouseMove (e) {
+  touches = !e.touches ? null : Array.prototype.slice.call(e.touches).map((touch) => {
+    return {
+      x: touch.clientX,
+      y: touch.clientY,
+      id: touch.identifier
+    }
+  })
   space.move(
     e.offsetX || e.clientX || (e.touches ? e.touches[0].clientX : 0),
     e.offsetY || e.clientY || (e.touches ? e.touches[0].clientY : 0),
-    e.shiftKey || (e.touches && e.touches.length === 2)
+    e.shiftKey || (e.touches && e.touches.length === 2),
+    touches
   )
 }
 
 function onMouseUp (e) {
+  touches = 0
   space.up()
 }
 
@@ -241,7 +351,6 @@ function onWheel (e) {
   )
   e.preventDefault()
 }
-
 
 // Currently this is a bit messy if the canvas is smaller than the window
 // and not at 0,0 because in onMouseMove the e.offsetX will be
@@ -258,4 +367,3 @@ window.addEventListener('touchend', onMouseUp)
 canvas.addEventListener('wheel', onWheel)
 
 space.changed.add(redraw)
-
